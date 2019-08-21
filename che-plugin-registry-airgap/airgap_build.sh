50b25618681c56ba82d0bf9d3e0b253cce9131aa
nickboldt <nboldt@redhat.com>
2019-08-21 10:12:48 -0400

    if we pass a url param to airgap_build.sh, it can now fix the Dockerfile to apply that registry URL for the replace_container_repos.sh script
    Change-Id: I5d850d327d4a2aeb64598422bfd8c2b2bb90bb4b
    Signed-off-by: nickboldt <nboldt@redhat.com>

--- che-plugin-registry-airgap/airgap_build.sh
+++ che-plugin-registry-airgap/airgap_build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-sed -i Dockerfile -e "s%# RUN ./fetch_resources.sh%RUN ./fetch_resources.sh%"
+sed -i Dockerfile -e "s%#.*RUN ./fetch_resources.sh%RUN ./fetch_resources.sh%"
+if [[ $1 ]]; then
+	sed -i Dockerfile -e "s%#.*RUN ./list_containers.sh v3%RUN ./list_containers.sh%"
+	sed -i Dockerfile -e "s%myquay.mycorp.com%${1}%"
+fi
+exit
 now=`date +%Y%m%d-%H%M`
 docker build . -t quay.io/nickboldt/airgap-che-plugin-registry:nightly --no-cache
 docker tag quay.io/nickboldt/airgap-che-plugin-registry:{nightly,$now}
