7b642a0118125e1f95f6aefe9f9c1a80af56cc82
nickboldt <nboldt@redhat.com>
2019-08-21 10:19:18 -0400

    when using custom registry, push to different tags
    Change-Id: I30bd0a3240f6a7e5d17fa6b0ea375efd02b2b3b7
    Signed-off-by: nickboldt <nboldt@redhat.com>

--- che-plugin-registry-airgap/airgap_build.sh
+++ che-plugin-registry-airgap/airgap_build.sh
@@ -3,12 +3,17 @@ sed -i Dockerfile -e "s%#.*RUN ./fetch_resources.sh%RUN ./fetch_resources.sh%"
 if [[ $1 ]]; then
 	sed -i Dockerfile -e "s%#.*RUN ./list_containers.sh v3%RUN ./list_containers.sh%"
 	sed -i Dockerfile -e "s%myquay.mycorp.com%${1}%"
+	nightly="${1%%.*}" # first section of the URL replacement
+	now="${nightly}-`date +%Y%m%d-%H%M`" # append timestamp
+else
+	nightly="nightly"
+	now=`date +%Y%m%d-%H%M`
 fi
 exit
 now=`date +%Y%m%d-%H%M`
-docker build . -t quay.io/nickboldt/airgap-che-plugin-registry:nightly --no-cache
-docker tag quay.io/nickboldt/airgap-che-plugin-registry:{nightly,$now}
-for d in nightly $now; do
+docker build . -t quay.io/nickboldt/airgap-che-plugin-registry:${nightly} --no-cache
+docker tag quay.io/nickboldt/airgap-che-plugin-registry:{${nightly},${now}}
+for d in ${nightly} ${now}; do
 	docker push quay.io/nickboldt/airgap-che-plugin-registry:${d} &
 done
 wait
