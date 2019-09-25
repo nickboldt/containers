6e4a1a8831237bdc8a67200ce24d3d431b23358e
nickboldt <nboldt@redhat.com>
2019-09-25 16:23:20 -0400

    attempt to serve index files if they're in a given folder (eg., redirect from plugins/ to plugins/index.json) but not working yet
    Change-Id: I6c7ada9eb5761af967a1b0d9b8d41885666c4802
    Signed-off-by: nickboldt <nboldt@redhat.com>

--- che-plugin-registry-https-go-minimal/main.go
+++ che-plugin-registry-https-go-minimal/main.go
@@ -1,17 +1,83 @@
+/* original source modified from 
+    https://golangcode.com/basic-https-server-with-certificate/ and 
+    https://golangcode.com/basic-docker-setup/ and  
+    https://dev.to/hauxe/golang-http-serve-static-files-correctly-2oj2
+*/ 
+
 package main
 
 import (
     "log"
 	"net/http"
 	"os"
-	"strings"
+    "strings"
 )
+
+// FileSystem custom file system handler
+type FileSystem struct {
+    fs http.FileSystem
+}
+
+// FileExists reports whether the named file exists as a boolean
+func FileExists(name string) bool {
+    if fi, err := os.Stat(name); err == nil {
+        if fi.Mode().IsRegular() {
+            return true
+        }
+    }
+    return false
+}
+
+// DirExists reports whether the dir exists as a boolean
+func DirExists(name string) bool {
+    if fi, err := os.Stat(name); err == nil {
+        if fi.Mode().IsDir() {
+            return true
+        }
+    }
+    return false
+}
+
+// TODO make this serve index files if they're found (not working yet)
+func (fs FileSystem) Open(path string) (http.File, error) {
+    if path != "/" && DirExists(path) {
+        indexfiles := []string{"index.json", "README.md", "meta.yaml"}
+        for _, indexfile := range indexfiles {
+            log.Println("** Check for " + indexfile + " in " + path)
+            if FileExists(strings.TrimSuffix(path, "/") + "/" + indexfile) {
+                path = strings.TrimSuffix(path, "/") + "/" + indexfile
+                log.Println("** Found " + path)
+                break
+            }
+        }
+    }
+
+    f, err := fs.fs.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+    if DirExists(path) || FileExists(path) {
+		index := path
+		if _, err := fs.fs.Open(index); err != nil {
+			return nil, err
+		}
+    }
+    
+    return f, nil
+}
 
 func main() {
 
 	path := "/" // path on web server
 	directory := "html" // path in container relative to this app
-	http.Handle("/", http.StripPrefix(strings.TrimRight(path, "/"), http.FileServer(http.Dir(directory))))
+
+    // simple dir server
+    http.Handle("/", http.StripPrefix(strings.TrimRight(path, "/"), http.FileServer(http.Dir(directory))))
+
+    // TODO: more complex dir server: show README and index files (not working)
+    // fileServer := http.FileServer(FileSystem{http.Dir(directory)})
+    // http.Handle("/", http.StripPrefix(strings.TrimRight(path, "/"), fileServer))
 
     port := os.Getenv("PORT")
     if port == "" {
